dd9765e50816da16685b097e3019f01a5a87a742
Anatolii Vorona <vorona.tolik@gmail.com>
2020-06-08 01:08:41 +0200

    added lemming

---
+++ cmd/lemmingd/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BOPOHA/hamsterd/internal/sscert"
+	"github.com/go-httpproxy/httpproxy"
+	"log"
+	"net/http"
+)
+
+type LemmingRule struct {
+	socket string
+	hosts  []string
+}
+
+var redirects = []LemmingRule{
+	{
+		socket: "127.0.0.1:8000",
+		hosts: []string{
+			"static.example1.com",
+			"static.as.example1.com",
+			"static.eu.example1.com",
+			"static.b2b.example1.com",
+			"static.stage.b2b.example1.com",
+			"static-stage.example1.com",
+			"static-qa01.example1.com",
+			"static-qa02.example1.com",
+			"static-qa03.example1.com",
+			"static-qa04.example1.com",
+			"static-qa05.example1.com",
+			"static-qa06.example1.com",
+			"static-qa07.example1.com",
+			"static-qa08.example1.com",
+			"static-qa09.example1.com",
+			"static-qa10.example1.com",
+			"static-qa11.example1.com",
+			"static-qa12.example1.com",
+			"static-qa16.example1.com",
+			"static.happify.localhost",
+			"static.happify.local",
+		},
+	},
+}
+
+func main() {
+
+	prx, err := httpproxy.NewProxyCert(sscert.CACert, sscert.CAKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	prx.Rt = GetNewLemmingTransport(redirects)
+	prx.OnError = OnError
+	prx.OnConnect = OnConnect
+	fmt.Println(string(sscert.CACert))
+	http.ListenAndServe(":18080", prx)
+}
+
+func OnError(ctx *httpproxy.Context, where string,
+	err *httpproxy.Error, opErr error) {
+	log.Printf("ERR: %s: %s [%s]", where, err, opErr)
+}
+
+func OnConnect(ctx *httpproxy.Context, host string) (
+	ConnectAction httpproxy.ConnectAction, newHost string) {
+	switch rt := (ctx.Prx.Rt).(type) {
+	case *LemmingTransport:
+		if _, ok := rt.HostMap[host]; ok {
+			return httpproxy.ConnectMitm, host
+		}
+	}
+	return httpproxy.ConnectProxy, host
+}
