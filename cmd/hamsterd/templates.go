0a3c12982f089a8fc1b0aa9b4470b7d46844b862
Anatolii Vorona <vorona.tolik@gmail.com>
2021-10-11 22:34:20 +0200

    refactoring with tpl

---
+++ cmd/hamsterd/templates.go
@@ -0,0 +1,41 @@
+package main
+
+const j2Index = `
+#!/bin/bash +x -e -u
+source /etc/os-release
+if [ "${ID}" = "fedora" ]; then
+	curl -s {{ .ReqHost }}{{ .CaURL }} -o /etc/pki/ca-trust/source/anchors/proxy.dev.crt
+	update-ca-trust
+	dnf config-manager \
+	  --setopt=proxy={{ .ReqHost }} \
+	  --setopt=best=False \
+	  --setopt=installonly_limit=3 \
+	  --setopt=clean_requirements_on_remove=True \
+	  --setopt=install_weak_deps=False \
+	  --setopt=fastestmirror=False \
+	  --save
+	
+	dnf config-manager \
+	  --setopt          'fedora.baseurl=https://d2lzkl7pfhq30w.cloudfront.net/pub/fedora/linux/releases/$releasever/Everything/$basearch/os/' \
+	  --setopt  'fedora-modular.baseurl=https://d2lzkl7pfhq30w.cloudfront.net/pub/fedora/linux/releases/$releasever/Modular/$basearch/os/'    \
+	  --setopt         'updates.baseurl=https://d2lzkl7pfhq30w.cloudfront.net/pub/fedora/linux/updates/$releasever/Everything/$basearch/'     \
+	  --setopt 'updates-modular.baseurl=https://d2lzkl7pfhq30w.cloudfront.net/pub/fedora/linux/updates/$releasever/Modular/$basearch/'        \
+	  --setopt          fedora.metalink=None \
+	  --setopt  fedora-modular.metalink=None \
+	  --setopt         updates.metalink=None \
+	  --setopt updates-modular.metalink=None \
+	  --setopt fedora-cisco-openh264.enabled=0 \
+	  --save
+	
+	dnf install -y \
+	  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${VERSION_ID}.noarch.rpm \
+	  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${VERSION_ID}.noarch.rpm
+	sed -i 's/^metalink=/#\0/; s/#baseurl=/baseurl=/' /etc/yum.repos.d/rpmfusion*repo
+fi
+echo Done
+`
+
+type paramsJ2Index struct {
+	ReqHost string
+	CaURL   string
+}
